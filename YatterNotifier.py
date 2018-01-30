7025346218441372b6b2987494bb6d9cc3ac8c02
Gordon Alexander MacPherson <gordon.a.macpherson@gmail.com>
2018-01-30 22:45:04 +0000

    Added systray icon for polling application and updated to use the proper Win32 application
    This should now run on Linux and Mac, but needs tested.

--- YatterNotifier.py
+++ YatterNotifier.py
@@ -2,22 +2,28 @@ import sched, time
 from lxml import html
 from collections import namedtuple
 from multiprocessing import Queue
+from functools import wraps
 import requests
 from plyer import notification as osnotify
 from decimal import Decimal
 from playsound import playsound
 from pathlib import Path
 from logging import basicConfig, info, DEBUG
+from queue import Queue
 import pyttsx3
 import json
 import os
+import pystray
+from threading import Thread
+from PIL import Image
 
 basicConfig(filename='polling.log', level=DEBUG)
 
 Engine = None
 
-def InitSpeechEngine():
 
+
+def InitSpeechEngine():
     global Engine
     for attempt in range(10):
         try:
@@ -62,6 +68,7 @@ def load_config():
             json.dump(config_new, json_data_file, sort_keys=True, indent=4)
             Config = config_new
 
+
 load_config()
 
 Debugging = Config['debugging_enabled']
@@ -73,36 +80,34 @@ def get_seedrs_profile():
     return html.fromstring(page.content)
 
 
-def get_investment_percentage( profile ):
+def get_investment_percentage(profile):
     return str(profile.xpath('//div[@class="CampaignProgress-percentage"]/text()')[0]).strip()
 
 
-def get_investment_count( profile ):
+def get_investment_count(profile):
     return str(profile.xpath('//span[@class="total"]/text()')[0]).strip()
 
 
-def get_current_investment_amount( profile ):
+def get_current_investment_amount(profile):
     amount = str(profile.xpath('//dl[@class="investment_already_funded"]/dd/text()')[0]).strip().replace(',', '')
     # convert to number
     return Decimal(amount[1:])
 
+
 # store initial percentage
 InvestorCountDelta = get_investment_count(get_seedrs_profile())
 
-
 print("Initial amount: ", get_current_investment_amount(get_seedrs_profile()))
-
 
 # debugging makes the alert run on startup and ignores trigger case
 
 InvestmentAmount = get_current_investment_amount(get_seedrs_profile())
 
 
-
 def request_investment_info():
     global InvestorCountDelta
     global InvestmentAmount
-    #global Engine
+    # global Engine
     global Config
     seedrs = get_seedrs_profile()
     current_investor_count = get_investment_count(seedrs)
@@ -127,14 +132,15 @@ def request_investment_info():
             # execute notification
             osnotify.notify(
                 title='Seedrs New Investment Received',
-                message='Investment amount received £' + str(received_amount) + ", you're now " + str(investment_percentage) + " percent funded. " + " Investment capital: £" + str(investment_amount),
+                message='Investment amount received £' + str(received_amount) + ", you're now " + str(
+                    investment_percentage) + " percent funded. " + " Investment capital: £" + str(investment_amount),
                 app_name='Investment',
                 app_icon='notification.ico'
             )
 
         # is configuration enabled for playing text to speech notification
         if Config['text_to_speech']:
-            Engine.say('Investment amount received: £' + str(received_amount)+ ".")
+            Engine.say('Investment amount received: £' + str(received_amount) + ".")
             Engine.say('Investment percentage is now: ' + str(investment_percentage) + ".")
             Engine.say('We now have: ' + get_investment_count(seedrs) + ' investors.')
             Engine.say('Our investment capital is now: £' + str(investment_amount))
@@ -154,15 +160,65 @@ osnotify.notify(
 )
 
 
+def investment_polling(queue):
+    info("executing work on separate thread")
+    s = sched.scheduler(time.time, time.sleep)
 
-s = sched.scheduler(time.time, time.sleep)
-def investment_update(sc):
-    info("polling for investment changes")
-    request_investment_info()
-    s.enter(PollingInterval, 1, investment_update, (sc,))
+    def investment_update(sc):
+        info("polling for investment changes")
+        request_investment_info()
+        data = queue.get()
+        if data != "exit":
+            s.enter(PollingInterval, 1, investment_update, (sc,))
 
-# wait 5 seconds before running first poll
-s.enter(5, 1, investment_update, (s,))
-s.run()
+    # wait 5 seconds before running first poll
+    s.enter(5, 1, investment_update, (s,))
+    s.run()
 
 
+def ui_updates(queue):
+    info("executing work for UI information")
+
+    def get_image_icon():
+        return Image.open("notification.ico")
+
+    icon_taskbar = pystray.Icon(
+        'Seedrs Notification Tool',
+        title="Seedrs Notification Tool"
+    )
+
+    icon_taskbar.icon = get_image_icon()
+
+    def exit():
+        """ exit application thread"""
+        icon_taskbar.stop()
+        queue.put("exit")
+
+    def configure_icon(icon):
+        icon.visible = True
+        item = pystray.MenuItem("Exit", exit)
+        icon.menu = pystray.Menu(item)
+
+    icon_taskbar.run(configure_icon)
+
+
+event_queue = Queue()
+
+threads = []
+work_thread = Thread(target=investment_polling, args=(event_queue,))
+threads.append(work_thread)
+
+ui_thread = Thread(target=ui_updates, args=(event_queue,))
+threads.append(ui_thread)
+
+# start worker and ui
+for thread in threads:
+    thread.start()
+
+# join thread
+for thread in threads:
+    thread.join()
+
+# ui_runner = threads[1]
+# when this thread exits the other will be forced to exit too
+# ui_runner.join()
--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import sys
 from cx_Freeze import setup, Executable
+import os.path
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
@@ -13,7 +17,10 @@ build_exe_options = {
         "plyer",
         "requests.compat",
         "urllib3",
-        "pyttsx3"
+        "pyttsx3",
+        "pystray",
+        "PIL",
+        "MicImagePlugin"
     ],
     "include_files": [
         "notification.ico",
@@ -23,10 +30,13 @@ build_exe_options = {
     "include_msvcr": True
 }
 
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
 
 setup(
     name = "Yatter Seedrs Notifier",
-    version = "0.1",
-    description = "A notification tool for the Seedrs platform",
+    version = "0.3",
+    description = "Seedrs investment notification",
     options = {"build_exe": build_exe_options},
-    executables = [Executable("YatterNotifier.py", icon='notification.ico')])
+    executables = [Executable("YatterNotifier.py", icon='notification.ico', base=base)])
